16136c7b3cd496e4484b3990e7390de40c74bb3e
Marciel Santana <marcyel.jsclaret17@gmail.com>
2026-01-21 10:20:45 -0300

    javascript- exercicios 1 e 2

---
+++ FUNDAMENTOS/javascript/prime-passos-js/exerc1.js
@@ -0,0 +1,19 @@
+let num1 = 6
+let num2 = 3
+
+let adicao = num1 + num2
+console.log(`A Soma dos números ${num1} e ${num2} é ${adicao}`);
+
+let multiplicacao = num1 * num2
+console.log(`A multiplicação dos números ${num1} e ${num2} é ${multiplicacao}`);
+
+let subtracao = num1 - num2
+console.log(`A Subtração dos números ${num1} e ${num2} é  ${subtracao}`);
+
+let divisao = num1 / num2
+console.log(`A Divisão dos números ${num1} e ${num2} é  ${divisao}`);
+
+let modulo = num1 % num2
+console.log(`O resto(módulo) dos números ${num1} e ${num2} é  ${subtracao}`);
+
+
---
+++ FUNDAMENTOS/javascript/prime-passos-js/exerc2.js
@@ -0,0 +1,8 @@
+let num1 = 60
+let num2 = 30
+
+if (num1 > num2){
+    console.log(`O PRIMEIRO número: ${num1} é MAIOR` );
+} else{
+    console.log(`O SEGUNDO número: ${num2} é MAIOR` );
+}
